1b6384715e961aef8946bf9339a27dc83a8cf0cc
王明昊 <wangminghao2002@126.com>
2023-04-11 08:57:58 +0000

    使用无人机机载摄像头手势识别控制飞行。
    Signed-off-by: 王明昊 <wangminghao2002@126.com>

---
+++ UAV_camera/window.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+Project Name: yolov5-jungong
+File Name: window.py.py
+Author: chenming
+Create Date: 2021/11/8
+Description：图形化界面，可以检测摄像头、视频和图片文件
+-------------------------------------------------
+"""
+# 应该在界面启动的时候就将模型加载出来，设置tmp的目录来放中间的处理结果
+import shutil
+import PyQt5.QtCore
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import threading
+import argparse
+import os
+import sys
+from pathlib import Path
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+import os.path as osp
+import control
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams, LoadDrone
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
+                           increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, time_sync
+
+
+# 添加一个关于界面
+# 窗口主类
+class MainWindow(QTabWidget):
+    # 基本配置不动，然后只动第三个界面
+    def __init__(self):
+        # 初始化界面
+        super().__init__()
+        self.setWindowTitle('123')
+        self.resize(1200, 800)
+        # 图片读取进程
+        self.output_size = 480
+        self.img2predict = ""
+        self.device = '0'
+        # # 初始化视频读取线程
+        self.vid_source = '0'  # 初始设置为摄像头
+        self.stopEvent = threading.Event()
+        self.webcam = True
+        self.stopEvent.clear()
+        self.model = self.model_load(weights="runs/train/exps/weights/best.pt",
+                                     device=self.device)  # todo 指明模型加载的位置的设备
+        self.initUI()
+        self.reset_vid()
+
+    '''
+    ***模型初始化***
+    '''
+
+    @torch.no_grad()
+    def model_load(self, weights="",  # model.pt path(s)
+                   device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+                   half=False,  # use FP16 half-precision inference
+                   dnn=False,  # use OpenCV DNN for ONNX inference
+                   ):
+        device = select_device(device)
+        half &= device.type != 'cpu'  # half precision only supported on CUDA
+        device = select_device(device)
+        model = DetectMultiBackend(weights, device=device, dnn=dnn)
+        stride, names, pt, jit, onnx = model.stride, model.names, model.pt, model.jit, model.onnx
+        # Half
+        half &= pt and device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
+        if pt:
+            model.model.half() if half else model.model.float()
+        print("模型加载完成!")
+        return model
+
+    '''
+    ***界面初始化***
+    '''
+
+    def initUI(self):
+        # 图片检测子界面
+        font_title = QFont('楷体', 16)
+        font_main = QFont('楷体', 14)
+
+        # 视频识别界面的逻辑比较简单，基本就从上到下的逻辑
+        vid_detection_widget = QWidget()
+        vid_detection_layout = QVBoxLayout()
+        self.webcam_detection_btn = QPushButton("摄像头实时监测")
+        self.vid_stop_btn = QPushButton("停止检测")
+        self.webcam_detection_btn.setFont(font_main)
+        self.vid_stop_btn.setFont(font_main)
+        self.vid_img = QLabel()
+        self.vid_img.setPixmap(QPixmap("images/UI/up.jpeg"))
+        self.vid_img.setAlignment(Qt.AlignCenter)
+        self.webcam_detection_btn.setStyleSheet("QPushButton{color:white}"
+                                                "QPushButton:hover{background-color: rgb(2,110,180);}"
+                                                "QPushButton{background-color:rgb(48,124,208)}"
+                                                "QPushButton{border:2px}"
+                                                "QPushButton{border-radius:5px}"
+                                                "QPushButton{padding:5px 5px}"
+                                                "QPushButton{margin:5px 5px}")
+        self.vid_stop_btn.setStyleSheet("QPushButton{color:white}"
+                                        "QPushButton:hover{background-color: rgb(2,110,180);}"
+                                        "QPushButton{background-color:rgb(48,124,208)}"
+                                        "QPushButton{border:2px}"
+                                        "QPushButton{border-radius:5px}"
+                                        "QPushButton{padding:5px 5px}"
+                                        "QPushButton{margin:5px 5px}")
+        self.webcam_detection_btn.clicked.connect(self.open_cam)
+        self.vid_stop_btn.clicked.connect(self.close_vid)
+        # 添加组件到布局上
+        vid_detection_layout.addWidget(self.vid_img)
+        vid_detection_layout.addWidget(self.webcam_detection_btn)
+        vid_detection_layout.addWidget(self.vid_stop_btn)
+        vid_detection_widget.setLayout(vid_detection_layout)
+
+        self.addTab(vid_detection_widget, '视频检测')
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self,
+                                     'quit',
+                                     "Are you sure?",
+                                     QMessageBox.Yes | QMessageBox.No,
+                                     QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            self.close()
+            event.accept()
+        else:
+            event.ignore()
+
+    def open_cam(self):
+        self.webcam_detection_btn.setEnabled(False)
+        self.vid_stop_btn.setEnabled(True)
+        self.vid_source = '0'
+        self.webcam = True
+        # 把按钮给他重置了
+        # print("GOGOGO")
+        th = threading.Thread(target=self.detect_vid)
+        th.start()
+
+
+    def detect_vid(self):
+        # pass
+        model = self.model
+        output_size = self.output_size
+        # source = self.img2predict  # file/dir/URL/glob, 0 for webcam
+        imgsz = [640, 640]  # inference size (pixels)
+        conf_thres = 0.25  # confidence threshold
+        iou_thres = 0.45  # NMS IOU threshold
+        max_det = 1000  # maximum detections per image
+        # device = self.device  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img = False  # show results
+        save_txt = False  # save results to *.txt
+        save_conf = False  # save confidences in --save-txt labels
+        save_crop = False  # save cropped prediction boxes
+        nosave = False  # do not save images/videos
+        classes = None  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms = False  # class-agnostic NMS
+        augment = False  # ugmented inference
+        visualize = False  # visualize features
+        line_thickness = 3  # bounding box thickness (pixels)
+        hide_labels = False  # hide labels
+        hide_conf = False  # hide confidences
+        half = False  # use FP16 half-precision inference
+        dnn = False  # use OpenCV DNN for ONNX inference
+        source = str(self.vid_source)
+        webcam = self.webcam
+        device = select_device(self.device)
+        stride, names, pt, jit, onnx = model.stride, model.names, model.pt, model.jit, model.onnx
+        imgsz = check_img_size(imgsz, s=stride)  # check image size
+        save_img = not nosave and not source.endswith('.txt')  # save inference images
+        # Dataloader
+        view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadDrone(source, img_size=imgsz, stride=stride, auto=pt and not jit)
+        dataset.Drone.takeoff()
+        bs = len(dataset)  # batch_size
+        if pt and device.type != 'cpu':
+            model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
+        dt, seen = [0.0, 0.0, 0.0], 0
+        for path, im, im0s, vid_cap, s in dataset:
+            t1 = time_sync()
+            im = torch.from_numpy(im).to(device)
+            im = im.half() if half else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+            t2 = time_sync()
+            dt[0] += t2 - t1
+            # Inference
+            # visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+            pred = model(im, augment=augment, visualize=visualize)
+            t3 = time_sync()
+            dt[1] += t3 - t2
+            # NMS
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+            dt[2] += time_sync() - t3
+            # Second-stage classifier (optional)
+            # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+            # Process predictions
+            for i, det in enumerate(pred):  # per image
+                seen += 1
+                if webcam:  # batch_size >= 1
+                    p, im0, frame = path[i], im0s[i].copy(), dataset.count
+                    s += f'{i}: '
+                else:
+                    p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+                p = Path(p)  # to Path
+                s += '%gx%g ' % im.shape[2:]  # print string
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                imc = im0.copy() if save_crop else im0  # for save_crop
+                annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        if save_txt:  # Write to file
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(
+                                -1).tolist()  # normalized xywh
+                            line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                            # with open(txt_path + '.txt', 'a') as f:
+                            #     f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                        if save_img or save_crop or view_img:  # Add bbox to image
+                            c = int(cls)  # integer class
+                            label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                            annotator.box_label(xyxy, label, color=colors(c, True))
+                            # if save_crop:
+                            #     save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg',
+                            #                  BGR=True)
+                # Print time (inference-only)
+                LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
+                print(s)
+                # Stream results
+                lr, fb, ud, yv = control.control_Tello(s,35)
+                print("控制信号：",lr,' ',fb,' ',ud,' ',yv)
+                dataset.Drone.send_rc_control(lr, fb, ud, yv)
+                # Save results (image with detections)
+                im0 = annotator.result()
+                frame = im0
+                resize_scale = output_size / frame.shape[0]
+                frame_resized = cv2.resize(frame, (0, 0), fx=resize_scale, fy=resize_scale)
+                cv2.imwrite("images/tmp/single_result_vid.jpg", frame_resized)
+                self.vid_img.setPixmap(QPixmap("images/tmp/single_result_vid.jpg"))
+
+            if cv2.waitKey(25) & self.stopEvent.is_set() == True:
+                self.stopEvent.clear()
+                self.webcam_detection_btn.setEnabled(True)
+                self.mp4_detection_btn.setEnabled(True)
+                self.reset_vid()
+                break
+
+    def reset_vid(self):
+        self.webcam_detection_btn.setEnabled(True)
+        self.vid_img.setPixmap(QPixmap("images/UI/up.jpeg"))
+        self.vid_source = '0'
+        self.webcam = True
+
+    def close_vid(self):
+        self.stopEvent.set()
+        self.reset_vid()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
